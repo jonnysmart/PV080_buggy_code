106b3355b22c4fb270718b9f9630f79840b0bc6c
jonnysmart <jonnysmart1@gmail.com>
2025-05-02 08:34:13 +0200

    update

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -38,8 +38,7 @@ def fetch_website(urllib_version, url):
 def load_yaml(filename):
     stream = open(filename)
     deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
-    return deserialized_data
-    
+    return deserialized_data 
 def authenticate(password):
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
